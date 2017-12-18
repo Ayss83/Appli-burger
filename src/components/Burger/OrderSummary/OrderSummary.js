1aaa0983bfc539f8b78993f5aadc51ddea548fe3
Renaud <renoruns@gmail.com>
2017-12-18 22:42:15 +0100

    Déplacement de Layout dans le dossier containers, optimisation rendu du composant Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
-import WrapperHoc from '../../../hoc/WrapperHoc';
+import Aux from '../../../hoc/WrapperHoc';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    
+const orderSummary = props => {
+
+    //Récupération des différents ingrédients et listing de ceux ci
     const ingredientSummary = Object.keys(props.ingredients)
     .map(ingKey => {
         return <li key={ingKey}>
                     <span style={{textTransform: "capitalize"}}>{ingKey}</span> : {props.ingredients[ingKey]}
                 </li>;
     })
-    
+
     return(
-    <WrapperHoc>
-        <h3>Votre commande</h3>
-        <p>Voici la liste des ingrédients que vous avez rajouté à votre burger : </p>
-        <ul>
-            {ingredientSummary}
-        </ul>
-        <p>Prix total : <strong>{props.price.toFixed(2)} €</strong></p>
-        <p>Confirmer la commande?</p>
-        <Button btnType="Danger" clicked={props.purchaseCanceled}>Annuler</Button>
-        <Button btnType="Success" clicked={props.purchaseContinue}>Commander</Button>
-    </WrapperHoc>)
-};
+        <Aux>
+            <h3>Votre commande</h3>
+            <p>Voici la liste des ingrédients que vous avez rajouté à votre burger : </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Prix total : <strong>{props.price.toFixed(2)} €</strong></p>
+            <p>Confirmer la commande?</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled}>Annuler</Button>
+            <Button btnType="Success" clicked={props.purchaseContinue}>Commander</Button>
+        </Aux>
+    )
+}
 
 export default orderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from './Modal.css';
 import WrapperHoc from '../../../hoc/WrapperHoc';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <WrapperHoc>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}
-        className={classes.Modal}>
-            {props.children}
-        </div>
-    </WrapperHoc>
-)
+class Modal extends Component {
 
-export default modal;
+    //le composant ne s'actualise que s'il y a un changement de sa propriété show
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <WrapperHoc>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}
+                className={classes.Modal}>
+                    {this.props.children}
+                </div>
+            </WrapperHoc>
+        )
+    }
+}
+
+export default Modal;
